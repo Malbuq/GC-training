97ce24ce5eef3bb1b4ed7a91cedaca3a88172fcb
RonyRangelSS <rony.rangel@upe.br>
2024-10-27 15:11:59 -0300

    fix: corração da linguagem

---
+++ Operacoes.java
@@ -0,0 +1,10 @@
+public class Operacoes {
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
+}
